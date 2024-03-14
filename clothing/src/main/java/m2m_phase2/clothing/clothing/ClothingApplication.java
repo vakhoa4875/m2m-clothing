dfa54582665138908948f3495f46794db0f39b57
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-03-14 20:55:37 +0700

    init

---
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/ClothingApplication.java
@@ -0,0 +1,13 @@
+package m2m_phase2.clothing.clothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothingApplication.class, args);
+	}
+
+}
---
+++ clothing/src/test/java/m2m_phase2/clothing/clothing/ClothingApplicationTests.java
@@ -0,0 +1,13 @@
+package m2m_phase2.clothing.clothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
