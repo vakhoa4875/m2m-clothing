41bf17a434f797182ad8d230af1a84faa84455b7
ChauPhat <hieuphung2111@gmail.com>
2024-03-17 12:37:36 +0700

    cc

--- clothing/src/main/java/m2m_phase2/clothing/clothing/service/impl/AccountServiceImpl.java
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/service/impl/AccountServiceImpl.java
@@ -1,23 +1,48 @@
 package m2m_phase2.clothing.clothing.service.impl;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import m2m_phase2.clothing.clothing.entity.Account;
 import m2m_phase2.clothing.clothing.repository.AccountRepo;
 import m2m_phase2.clothing.clothing.service.AccountService;
 
+import javax.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
 @Service
 public class AccountServiceImpl implements AccountService {
 
 	@Autowired
 	private AccountRepo repo;
+	@Autowired
+	private JavaMailSender emailSender;
 
 	@Override
 	public Account saveAccount(Account account) {
 		// TODO Auto-generated method stub
 		return repo.save(account);
+	}
+	
+	
+	
+	public void sendOTPEmail(String toEmail, String otp) {
+	    try {
+	        MimeMessage message = emailSender.createMimeMessage();
+	        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+	        helper.setFrom("your_email_address@gmail.com");
+	        helper.setTo(toEmail);
+	        helper.setSubject("Mã OTP cho đăng ký tài khoản");
+	        helper.setText("Mã OTP của bạn là: " + otp);
+
+	        emailSender.send(message);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        // Xử lý lỗi khi gửi email
+	    }
 	}
 	
 	
