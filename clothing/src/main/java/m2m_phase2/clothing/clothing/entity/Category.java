3b037a96eda84c1728d1a4808ef24fae326688aa
TanLoc060704 <128579533+TanLoc060704@users.noreply.github.com>
2024-03-17 12:53:19 +0700

    create class entity category and product - TanLoc

---
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/entity/Category.java
@@ -0,0 +1,79 @@
+package m2m_phase2.clothing.clothing.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Category")
+public class Category implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "category_id")
+	private int category_id ;
+	
+	@Column(name = " category_name" ,  nullable =  false, unique = true)
+	private String category_name;
+	
+	@Column(name = "logo")
+	private String logo;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@OneToMany(mappedBy = "category")
+	private List<Product> produts;
+
+	public int getCategory_id() {
+		return category_id;
+	}
+
+	public void setCategory_id(int category_id) {
+		this.category_id = category_id;
+	}
+
+	public String getCategory_name() {
+		return category_name;
+	}
+
+	public void setCategory_name(String category_name) {
+		this.category_name = category_name;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Product> getProduts() {
+		return produts;
+	}
+
+	public void setProduts(List<Product> produts) {
+		this.produts = produts;
+	}
+	
+	
+	
+	
+	
+}
