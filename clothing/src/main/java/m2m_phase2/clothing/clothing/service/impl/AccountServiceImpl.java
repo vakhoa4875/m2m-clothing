1272d6b2d1136c1c9bd24aa48d49951be63162bc
ChauPhat <hieuphung2111@gmail.com>
2024-03-17 12:48:05 +0700

    cc2

--- clothing/src/main/java/m2m_phase2/clothing/clothing/service/impl/AccountServiceImpl.java
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/service/impl/AccountServiceImpl.java
@@ -8,17 +8,17 @@ import m2m_phase2.clothing.clothing.entity.Account;
 import m2m_phase2.clothing.clothing.repository.AccountRepo;
 import m2m_phase2.clothing.clothing.service.AccountService;
 
-import javax.mail.internet.MimeMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+//import javax.mail.internet.MimeMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.mail.javamail.MimeMessageHelper;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
 	@Autowired
 	private AccountRepo repo;
-	@Autowired
-	private JavaMailSender emailSender;
+//	@Autowired
+//	private JavaMailSender emailSender;
 
 	@Override
 	public Account saveAccount(Account account) {
@@ -28,22 +28,22 @@ public class AccountServiceImpl implements AccountService {
 	
 	
 	
-	public void sendOTPEmail(String toEmail, String otp) {
-	    try {
-	        MimeMessage message = emailSender.createMimeMessage();
-	        MimeMessageHelper helper = new MimeMessageHelper(message, true);
-
-	        helper.setFrom("your_email_address@gmail.com");
-	        helper.setTo(toEmail);
-	        helper.setSubject("Mã OTP cho đăng ký tài khoản");
-	        helper.setText("Mã OTP của bạn là: " + otp);
-
-	        emailSender.send(message);
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        // Xử lý lỗi khi gửi email
-	    }
-	}
+//	public void sendOTPEmail(String toEmail, String otp) {
+//	    try {
+//	        MimeMessage message = emailSender.createMimeMessage();
+//	        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+//
+//	        helper.setFrom("your_email_address@gmail.com");
+//	        helper.setTo(toEmail);
+//	        helper.setSubject("Mã OTP cho đăng ký tài khoản");
+//	        helper.setText("Mã OTP của bạn là: " + otp);
+//
+//	        emailSender.send(message);
+//	    } catch (Exception e) {
+//	        e.printStackTrace();
+//	        // Xử lý lỗi khi gửi email
+//	    }
+//	}
 	
 	
 
