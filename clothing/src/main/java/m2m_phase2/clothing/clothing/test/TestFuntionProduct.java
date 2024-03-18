b57a3c2fe5c6dd55244281e598d821cf1f48eeea
TanLoc060704 <128579533+TanLoc060704@users.noreply.github.com>
2024-03-18 20:28:25 +0700

    change - TanLoc

--- clothing/src/main/java/m2m_phase2/clothing/clothing/test/TestFuntionProduct.java
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/test/TestFuntionProduct.java
@@ -1,5 +1,6 @@
 package m2m_phase2.clothing.clothing.test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,26 +23,42 @@ public class TestFuntionProduct {
 	@GetMapping("/testfuntionproduct")
 	public String testfuntionproduct(Model model) {
 		
-	model.addAttribute("products", productServiceImpl.findAll()) ;
+
 	
-	
-		 List<Product> list =   productServiceImpl.findAll();
+
 		 
-		 String pictures = list.get(0).getPictures();
-		 System.out.println(pictures);
-		 
-		 String [] ArrayPictures = pictures.split(",");
-		 
-		 for (String string : ArrayPictures) {
-			System.out.println(string);
-		 }
-		 
-		 model.addAttribute("pictures",ArrayPictures);
+//		 String pictures = list.get(0).getPictures();
+//		 System.out.println(pictures);
+//		 
+//		 String [] ArrayPictures = pictures.split(",");
+//		 
+//		 for (String string : ArrayPictures) {
+//			System.out.println(string);
+//		 }
+//		 System.out.println(ArrayPictures[0]);
+//		 model.addAttribute("pictures",ArrayPictures); 
 //		 for (Product product : list) {
 //			System.out.println(list.toString()+"\n");
 //			System.out.println("-------------------------------");
 //		}
-		 return "Front_End/SanPham";
+		 
+ 		List<Product> list =   productServiceImpl.findAll();
+ 		List<String> firstPictures = new ArrayList<>();
+		 
+		 
+ 		for (Product product : list) {
+ 			String pathPictures = product.getPictures();
+			String[] arrayPictures = pathPictures.split(",");
+			if(arrayPictures.length > 0) {
+				firstPictures.add(arrayPictures[0]);
+			}
+ 		}
+ 		
+ 		model.addAttribute("products", list) ;
+ 		model.addAttribute("firstimg",firstPictures);
+		
+ 		
+		return "Front_End/SanPham";
 	}
 	
 }
