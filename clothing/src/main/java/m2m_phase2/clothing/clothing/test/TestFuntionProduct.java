3d4a37757fa256eb1b677fb2d5ca813be9aca63a
TanLoc060704 <128579533+TanLoc060704@users.noreply.github.com>
2024-03-18 16:15:44 +0700

    coding funtion load all product _ TanLoc

--- clothing/src/main/java/m2m_phase2/clothing/clothing/test/TestFuntionProduct.java
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/test/TestFuntionProduct.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 import m2m_phase2.clothing.clothing.entity.Product;
 import m2m_phase2.clothing.clothing.service.ProductService;
@@ -19,11 +20,27 @@ public class TestFuntionProduct {
 	private ProductServiceImpl productServiceImpl;
 	
 	@GetMapping("/testfuntionproduct")
-	public String testfuntionproduct() {
+	public String testfuntionproduct(Model model) {
+		
+	model.addAttribute("products", productServiceImpl.findAll()) ;
+	
+	
 		 List<Product> list =   productServiceImpl.findAll();
-		 for (Product product : list) {
-			System.out.println(list.toString());
-		}
+		 
+		 String pictures = list.get(0).getPictures();
+		 System.out.println(pictures);
+		 
+		 String [] ArrayPictures = pictures.split(",");
+		 
+		 for (String string : ArrayPictures) {
+			System.out.println(string);
+		 }
+		 
+		 model.addAttribute("pictures",ArrayPictures);
+//		 for (Product product : list) {
+//			System.out.println(list.toString()+"\n");
+//			System.out.println("-------------------------------");
+//		}
 		 return "Front_End/SanPham";
 	}
 	
