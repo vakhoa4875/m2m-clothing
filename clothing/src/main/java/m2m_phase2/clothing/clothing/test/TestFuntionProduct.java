7553bfd8e637aba5e294575bc72889ff4622f9d2
TanLoc060704 <128579533+TanLoc060704@users.noreply.github.com>
2024-03-19 13:20:17 +0700

    coding detail -TanLoc

--- clothing/src/main/java/m2m_phase2/clothing/clothing/test/TestFuntionProduct.java
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/test/TestFuntionProduct.java
@@ -11,6 +11,7 @@ import m2m_phase2.clothing.clothing.entity.Product;
 import m2m_phase2.clothing.clothing.service.ProductService;
 import m2m_phase2.clothing.clothing.service.impl.ProductServiceImpl;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -22,43 +23,34 @@ public class TestFuntionProduct {
 	
 	@GetMapping("/testfuntionproduct")
 	public String testfuntionproduct(Model model) {
-		
-
-	
-
-		 
-//		 String pictures = list.get(0).getPictures();
-//		 System.out.println(pictures);
+			 
+		List<Product> list =   productServiceImpl.findAll();
+// 		List<String> firstPictures = new ArrayList<>();
 //		 
-//		 String [] ArrayPictures = pictures.split(",");
 //		 
-//		 for (String string : ArrayPictures) {
-//			System.out.println(string);
-//		 }
-//		 System.out.println(ArrayPictures[0]);
-//		 model.addAttribute("pictures",ArrayPictures); 
-//		 for (Product product : list) {
-//			System.out.println(list.toString()+"\n");
-//			System.out.println("-------------------------------");
-//		}
-		 
- 		List<Product> list =   productServiceImpl.findAll();
- 		List<String> firstPictures = new ArrayList<>();
-		 
-		 
- 		for (Product product : list) {
- 			String pathPictures = product.getPictures();
-			String[] arrayPictures = pathPictures.split(",");
-			if(arrayPictures.length > 0) {
-				firstPictures.add(arrayPictures[0]);
-			}
- 		}
+// 		for (Product product : list) {
+// 			String pathPictures = product.getPictures();
+//			String[] arrayPictures = pathPictures.split(",");
+//			if(arrayPictures.length > 0) {
+//				firstPictures.add(arrayPictures[0]);
+//			}
+// 		}
+// 		model.addAttribute("firstimg",firstPictures); 
  		
  		model.addAttribute("products", list) ;
- 		model.addAttribute("firstimg",firstPictures);
 		
  		
 		return "Front_End/SanPham";
 	}
+	
+	@GetMapping("/product/{id}")
+	public String getDetail(@PathVariable Integer id, Model model ) {
+		
+		Product product = productServiceImpl.findByproductId(id);
+		model.addAttribute("product",product);
+
+		return "Front_End/ChiTietSanPham";
+	}
+	
 	
 }
