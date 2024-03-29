c7fefbf6e63fcff2548f915cef4ecf324c2ffdf4
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-03-29 17:39:35 +0700

    conflict resolved

--- clothing/src/main/java/m2m_phase2/clothing/clothing/controller/api/AdminApi.java
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/controller/api/AdminApi.java
@@ -1,5 +1,19 @@
 package m2m_phase2.clothing.clothing.controller.api;
 
-public class AdminApi {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import m2m_phase2.clothing.clothing.service.impl.UserServiceImpl;
+
+@RestController
+@RequestMapping("/api-admin/users")
+public class AdminApi {
+	
+//	final UserServiceImpl userServ;
+
+//	@GetMapping
+//	public ResponseObject<?> doGetAll
+	
+	
 }
