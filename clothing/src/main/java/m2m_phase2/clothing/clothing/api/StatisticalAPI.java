8c640d336ab7332ba7a6850cbe3689cc4cb4865e
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-05-16 19:34:09 +0700

    027_Commit03_Khoa: Fix YML

--- clothing/src/main/java/m2m_phase2/clothing/clothing/api/StatisticalAPI.java
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/api/StatisticalAPI.java
@@ -4,10 +4,12 @@ package m2m_phase2.clothing.clothing.api;
 import m2m_phase2.clothing.clothing.service.AccountService;
 import m2m_phase2.clothing.clothing.service.CategoryService;
 import m2m_phase2.clothing.clothing.service.ProductService;
-import m2m_phase2.clothing.clothing.service.impl.AccountServiceImpl;
+import m2m_phase2.clothing.clothing.service.StatisticService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -20,32 +22,44 @@ public class StatisticalAPI {
     ProductService productService;
     @Autowired
     CategoryService categoryService;
+    @Autowired
+    private StatisticService statisticService;
 
     @GetMapping("/api/getAccountData")
-    public Long AllAccount(){
+    public Long AllAccount() {
         return accountService.getTotalAccounts();
     }
+
     @PostMapping("/api/postAccountDK")
-    public Long accountDK(){
+    public Long accountDK() {
         return accountService.getDKAccount();
     }
 
     @PostMapping("/api/postAccountGG")
-    public Long accountGG(){
+    public Long accountGG() {
         return accountService.getGGAccount();
     }
 
     @GetMapping("/api/getProduct")
-    public Long AllProduct(){
+    public Long AllProduct() {
         return productService.getAllProducts();
     }
 
     @GetMapping("/api/getCategory")
-    public Long AllCategory(){
+    public Long AllCategory() {
         return categoryService.Category();
     }
+
     @GetMapping("/api/getProductCountPerCategory")
     public Map<String, Long> getProductCountPerCategory() {
         return categoryService.getProductCountPerCategory();
     }
+
+    // 028_Khoa
+    @GetMapping("/api-admin/getTop10SoldProduct")
+    public ResponseEntity<?> doGetTop10SoldProduct(@RequestParam("year") Integer year, @RequestParam("month") Integer month) {
+        var top10 = statisticService.getTop10SoldProductByMonthAndYear(month, year);
+        return ResponseEntity.ok(top10);
+    }
+    //
 }
