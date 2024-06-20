6502add3d324a4d5f8d8fb7507d670846bcf812a
khoa <128579628+vakhoa4875@users.noreply.github.com>
2024-06-20 14:15:35 +0700

    fix: reformat sql script + run flow pt2

--- clothing/src/main/java/m2m_phase2/clothing/clothing/repository/OrderDetailRepo.java
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/repository/OrderDetailRepo.java
@@ -1,17 +1,17 @@
-//package m2m_phase2.clothing.clothing.repository;
-//
-//import jakarta.transaction.Transactional;
-//import m2m_phase2.clothing.clothing.data.entity.Order;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Modifying;
-//import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.repository.query.Param;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface OrderDetailRepo extends JpaRepository<Order, Integer> {
-//    @Modifying
-//    @Transactional
-//    @Query(value = "UPDATE [order_detail] SET nameproduct = :nameproduct, quatity = :quatity, toal_product = :toal_product WHERE order_id_detail = :order_id_detail", nativeQuery = true)
-//    void UpdateOderDetail(@Param("order_id_detail") Integer order_id_detail, @Param("nameproduct") String nameproduct, @Param("quatity") Integer quatity, @Param("toal_product") Float toal_product);
-//}
+package m2m_phase2.clothing.clothing.repository;
+
+import jakarta.transaction.Transactional;
+import m2m_phase2.clothing.clothing.data.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailRepo extends JpaRepository<Order, Integer> {
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE [order_detail] SET nameproduct = :nameproduct, quatity = :quatity, toal_product = :toal_product WHERE order_id_detail = :order_id_detail", nativeQuery = true)
+    void UpdateOderDetail(@Param("order_id_detail") Integer order_id_detail, @Param("nameproduct") String nameproduct, @Param("quatity") Integer quatity, @Param("toal_product") Float toal_product);
+}
