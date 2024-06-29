c5796205c661670678821ace7b1412df85d9a181
ChauPhat <hieuphung2111@gmail.com>
2024-06-29 20:48:07 +0700

    chauphat: fix login and register

--- clothing/src/main/java/m2m_phase2/clothing/clothing/api/AccountApi.java
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/api/AccountApi.java
@@ -4,13 +4,17 @@ import jakarta.servlet.http.HttpSession;
 import m2m_phase2.clothing.clothing.constant.AccountEnum;
 import m2m_phase2.clothing.clothing.data.dto.AccountDto;
 import m2m_phase2.clothing.clothing.data.entity.Account;
+import m2m_phase2.clothing.clothing.data.variable.StaticVariable;
 import m2m_phase2.clothing.clothing.service.AccountService;
 import m2m_phase2.clothing.clothing.service.impl.AccountServiceImpl;
+import m2m_phase2.clothing.clothing.utils.PasswordEncoderUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AccountApi {
@@ -19,12 +23,133 @@ public class AccountApi {
     AccountServiceImpl accountImpl;
 
     @Autowired
-    AccountService account;
+    HttpSession session;
 
     @GetMapping("/getAccountByUsernameAndEmail")
     public Account findByUsernameAndEmail(@RequestParam("username") String username, @RequestParam("email") String email) {
         return accountImpl.findByUsernameAndEmail(username, email);
     }
+
+    @PostMapping("/api/account/getAccountByUsernameAndEmail")
+    public ResponseEntity<?> findByUsernameAndEmail2(@RequestBody AccountDto accountDto) {
+        Map<String, Object> response = new HashMap<>();
+        System.out.println(accountDto.getUsername());
+        try {
+            if (accountImpl.findByusername(accountDto.getUsername()) != null) {
+                response.put("data", null);
+                response.put("message", "Username already exists");
+                response.put("status", true);
+                return ResponseEntity.ok(response);
+            }
+            if (accountImpl.findByemail(accountDto.getEmail()) != null) {
+                response.put("data", null);
+                response.put("message", "Email already exists");
+                response.put("status", true);
+                return ResponseEntity.ok(response);
+            }
+            response.put("data", null);
+            response.put("message", "OTP sent to email");
+            response.put("status", true);
+            String otp = accountImpl.generateOTP();
+            accountImpl.sendOTPEmail(accountDto.getEmail(), otp, "OTP for Registering Account");
+            session.setAttribute("otpRegisterAccount", otp);
+            session.setAttribute("otpTimeLimit", System.currentTimeMillis());
+            session.setAttribute("accountRegister", accountDto);
+            session.setMaxInactiveInterval(60);
+        } catch (Exception e) {
+            response.put("data", null);
+            response.put("message", "Call API Failed");
+            response.put("status", false);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/api/account/verifyOtpRegister")
+    public ResponseEntity<?> verifyOtpRegister(@RequestParam("otp") String otp) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            String sessionOtp = (String) session.getAttribute("otpRegisterAccount");
+            long sessionTimeLimit = (long) session.getAttribute("otpTimeLimit");
+            if (sessionOtp != null && sessionOtp.equals(otp) && System.currentTimeMillis() - sessionTimeLimit < 60000) {
+                response.put("data", null);
+                response.put("message", "OTP is correct");
+                response.put("status", true);
+                AccountDto accountDto = (AccountDto) session.getAttribute("accountRegister");
+                accountImpl.saveAccount(AccountDto.convertAccountDtoToAccount(accountDto));
+            } else if (System.currentTimeMillis() - sessionTimeLimit > 60000) {
+                response.put("data", null);
+                response.put("message", "OTP is expired");
+                response.put("status", true);
+            } else if (sessionOtp != null && !sessionOtp.equals(otp)) {
+                response.put("data", null);
+                response.put("message", "OTP is incorrect");
+                response.put("status", true);
+            }
+        } catch (Exception e) {
+            response.put("data", null);
+            response.put("message", "Call API Failed");
+            response.put("status", false);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/api/account/resendOtpRegister")
+    public ResponseEntity<?> resendOtpRegister() {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            AccountDto accountDto = (AccountDto) session.getAttribute("accountRegister");
+            String otp = accountImpl.generateOTP();
+            accountImpl.sendOTPEmail(accountDto.getEmail(), otp, "OTP for Registering Account");
+            session.setAttribute("otpRegisterAccount", otp);
+            session.setAttribute("otpTimeLimit", System.currentTimeMillis());
+            session.setMaxInactiveInterval(60);
+            response.put("data", null);
+            response.put("message", "OTP sent to email");
+            response.put("status", true);
+        } catch (Exception e) {
+            response.put("data", null);
+            response.put("message", "Call API Failed");
+            response.put("status", false);
+        }
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/api/account/submitLogin")
+    public ResponseEntity<?> submitLogin(@RequestBody AccountDto accountDto) {
+        Map<String, Object> response = new HashMap<>();
+        try {
+            if(accountImpl.findByemail(accountDto.getEmail()) == null) {
+                response.put("data", null);
+                response.put("message", "Email does not exists");
+                response.put("status", true);
+                return ResponseEntity.ok(response);
+            }
+            Account account = accountImpl.findByemail(accountDto.getEmail());
+            boolean checkPass = PasswordEncoderUtil.verifyPassword(accountDto.getPassword(), account.getHashedPassword());
+            if (checkPass) {
+                response.put("data", null);
+                response.put("message", "Login success");
+                response.put("status", true);
+                // Lưu thông tin đăng nhập vào session hoặc làm bất kỳ xử lý nào khác cần thiết
+                session.setAttribute("loggedInUser", account.getEmail());
+                StaticVariable.sessionEmail = account.getEmail();
+                session.setAttribute("iduser", account.getUserId());
+                session.setAttribute("email", account.getEmail());
+                PasswordEncoderUtil.email = account.getEmail();
+            } else {
+                response.put("data", null);
+                response.put("message", "Password is incorrect");
+                response.put("status", true);
+            }
+
+        } catch (Exception e) {
+            response.put("data", null);
+            response.put("message", "Call API Failed");
+            response.put("status", false);
+        }
+        return ResponseEntity.ok(response);
+    }
+
 
     @PostMapping("/api-admin/postCreateAccount")
     public ResponseEntity<?> doPostCreateAccount(@RequestBody AccountDto accountDto) {
@@ -36,6 +161,7 @@ public class AccountApi {
         }
         return ResponseEntity.ok(insertStatus);
     }
+
     @GetMapping("/api-public/user/checkLoginStatus")
     @ResponseBody
     public boolean isLoggedIn(HttpSession session) {
@@ -45,16 +171,5 @@ public class AccountApi {
         // Kiểm tra nếu thông tin người dùng không null và là một chuỗi không rỗng
         return loggedInUser instanceof String && !((String) loggedInUser).isEmpty();
     }
-//    @GetMapping("/getUserByUsernameAndEmail")
-//    public ResponseEntity<?> doGetUserByUsernameAndEmail(UserDto userDto) {
-//        UserM user;
-//        try {
-//            user = account.getUserByUsernameAndEmail(userDto);
-//        } catch (SQLException e) {
-//            System.out.println("Call API Failed: /api-public/users/getUserByUsernameAndEmail");
-//            throw new RuntimeException(e);
-//        }
-//        return ResponseEntity.ok(user);
-//    }
 
 }
--- clothing/src/main/java/m2m_phase2/clothing/clothing/controller/RegisterController.java
+++ clothing/src/main/java/m2m_phase2/clothing/clothing/controller/RegisterController.java
@@ -1,66 +1,19 @@
 package m2m_phase2.clothing.clothing.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import m2m_phase2.clothing.clothing.data.entity.Account;
-import m2m_phase2.clothing.clothing.service.impl.AccountServiceImpl;
-import m2m_phase2.clothing.clothing.utils.PasswordEncoderUtil;
 
 @Controller
 public class RegisterController {
-
-    @Autowired
-    private AccountServiceImpl accountServiceImpl;
-    @Autowired
-    private HttpSession session;
-
     @GetMapping("/register") // hàm phatteacher
-    public String getHome(Model model) {
-
-        Account account = new Account();
-        model.addAttribute("account", account);
-        return "swappa/assests/html/acc_register";
+    public String register() {
+        return "swappa/assests/html/acc_register2";
 
     }
 
-    @PostMapping("/submitRegister") // hàm phatteacher
-    public String submitRegister(HttpServletRequest req, Model model) {
-        Account accountRegister = new Account();
-        accountRegister.setUsername(req.getParameter("firstName"));
-        accountRegister.setEmail(req.getParameter("email2"));
-        accountRegister.setHashedPassword(req.getParameter("password21"));
-        session.setAttribute("accountRegister", accountRegister);
-        String otp = accountServiceImpl.generateOTP();
-        accountServiceImpl.sendOTPEmail(accountRegister.getEmail(), otp, "OTP for Registering Account");
-        session.setAttribute("otp", otp);
-        return "swappa/assests/html/acc_register_OTP";
-    }
-
-    @PostMapping("/otp") // hàm phatteacher
-    public String otp(Model model, HttpServletRequest request) {
-        String otp1 = request.getParameter("otp1");
-        String otp2 = request.getParameter("otp2");
-        String otp3 = request.getParameter("otp3");
-        String otp4 = request.getParameter("otp4");
-        String otp5 = request.getParameter("otp5");
-        String otp6 = request.getParameter("otp6");
-        String otpSuccess = accountServiceImpl.concatOtp(otp1, otp2, otp3, otp4, otp5, otp6);
-        Account accountSession = (Account) session.getAttribute("accountRegister");
-        String sessionOTP = (String) session.getAttribute("otp");
-        if (otpSuccess.equals(sessionOTP)) {
-            String hashCode = accountSession.getHashedPassword();
-            accountSession.setHashedPassword(PasswordEncoderUtil.encodePassword(hashCode));
-            accountServiceImpl.saveAccount(accountSession);
-            return "swappa/assests/html/acc_login";
-        } else {
-            return "swappa/assests/html/acc_register_OTP";
-        }
+    @GetMapping("/verifyOtpRegister") // hàm phatteacher
+    public String verifyOtpRegister() {
+        return "swappa/assests/html/acc_register__OTP2";
 
     }
 
